71cd20bfdcd7344836d83edd4d0ce6d2b7781e46
katayama <tattu.0310@gmail.com>
2022-10-04 01:09:42 +0900

    ✨ for

--- newFor.js
+++ newFor.js
@@ -6,3 +6,10 @@ const say = () => {
 for (let i = 0; i < 10; i++, say()) {
     console.log(i);
 }
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//拡張for文
+for (let i of array) {
+    console.log(i);
+}
