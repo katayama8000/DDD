a75acf1e0a5c65075a4922689607220b7d10fd74
katayama8000 <tattu.0310@gmail.com>
2023-02-15 00:04:27 +0900

    repo

--- D-tech/src/infrastructure/DB/UserRepository.ts
+++ D-tech/src/infrastructure/DB/UserRepository.ts
@@ -1,22 +1,74 @@
 import { User } from "../../domain/entities/User.class";
 import { UserRepository as UserRepositoryInterface } from "../../domain/repositories/UserRepository.interface";
+
+const dummyUser1 = {
+  id: 1,
+  name: "dummy",
+  email: "test1@test.com",
+  password: "password",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+
+const dummyUser2 = {
+  id: 2,
+  name: "dummy2",
+  email: "test2@test.com",
+  password: "password",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+
+const dummyUser = [dummyUser1, dummyUser2];
 export class UserRepository implements UserRepositoryInterface {
   findUserAll(): Promise<User[]> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      const users = dummyUser.map((user) => User.create(user));
+      resolve(users);
+    });
   }
   findUserById(id: number): Promise<User | undefined> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      const user = dummyUser.find((user) => user.id === id);
+      if (user) {
+        resolve(User.create(user));
+      } else {
+        resolve(undefined);
+      }
+    });
   }
   findUserByEmail(email: string): Promise<User | undefined> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      const user = dummyUser.find((user) => user.email === email);
+      if (user) {
+        resolve(User.create(user));
+      } else {
+        resolve(undefined);
+      }
+    });
   }
-  createUser(user: User): Promise<User> {
-    throw new Error("Method not implemented.");
+  
+  createUser(user: User): Promise<User>{
+    const newUser = {
+      id: dummyUser.length + 1,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+    dummyUser.push(newUser);
+    return new Promise((resolve, reject) => {
+      resolve(User.create(newUser));
+    } 
+    );
   }
+  
   updateUser(user: User): Promise<User> {
     throw new Error("Method not implemented.");
   }
   deleteUser(id: string): Promise<void> {
     throw new Error("Method not implemented.");
   }
+
 }
