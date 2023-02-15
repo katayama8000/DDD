be34e97cd17402e77bf8c810ca3a99724eaf00e7
katayama8000 <tattu.0310@gmail.com>
2023-02-15 23:53:53 +0900

    update

--- D-tech/src/usecase/FetchUserAllUseCase.ts
+++ D-tech/src/usecase/FetchUserAllUseCase.ts
@@ -1,16 +1,15 @@
-import { inject, injectable } from "tsyringe";
-import { User } from "../domain/entities/User.class";
-import { UserRepository } from "../domain/repositories/UserRepository.interface";
+import { inject, injectable } from 'tsyringe';
+import { UserRepository } from '../domain/repositories/UserRepository.interface';
 import { UserDto } from './dto/User.dto';
 
 @injectable()
 export class FetchUserAllUseCase {
   constructor(
     @inject('UserRepository')
-    private userRepository: UserRepository,
+    private userRepository: UserRepository
   ) {}
 
-    async execute(): Promise<UserDto[]> {
-      return await this.userRepository.findUserAll();
+  async execute(): Promise<UserDto[]> {
+    return await this.userRepository.findUserAll();
   }
 }
